beb05ec4682af26b37d7f1b4d596c70ab9f86271
tge <tgeconf@gmail.com>
2020-10-27 16:36:30 +0800

    init

---
+++ src/main/index.js
@@ -0,0 +1,11 @@
+export default class GeTool {
+    constructor() {
+        
+    }
+
+    test() {
+        console.log('this is a test! ');
+    }
+}
+
+//export { ActionSpec, TimingSpec, ChartSpec, Animation }
